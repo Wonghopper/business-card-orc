e4b54080ab829c91652886383bc1ae565de1ccca
Wonghopper <jswong@DESKTOP-V9MT4TQ.localdomain>
2019-09-22 02:06:09 -0400

    Improved comments

--- business_card_parser.java
+++ business_card_parser.java
@@ -1,11 +1,13 @@
 /*
- * 
+ * This program is for the Asymmetrik coding challenge Business Card OCR. The 
+ * program takes the text of a business card as an input and outputs the name, 
+ * phone number, and email address of the owner of the business card.
  * Date Created: 9/21/19
  * Last Updated: 9/21/19
  * Last Update: 
- * TODO: 
+ * TODO: Add name parser
  */
- 
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
@@ -16,6 +18,11 @@ interface ContactInfo {
     String getEmailAddress();
 }
 
+/*
+ * This class defines the Contact object. The object stores the information 
+ * parsed from a business card. The class also defines the methods that parse 
+ * the business card text for information.
+ */
 class Contact implements ContactInfo {
     String name = "Default Name";
     String phoneNumber = "Default Number";
@@ -36,9 +43,19 @@ class Contact implements ContactInfo {
     }
 
     public String parseNumber(String text) {
-        //Assuming North American Numbering Plan
+        // Assuming North American Numbering Plan
         // Create regex to pattern match phone numbers
-        String regex = "([A-Za-z]*[ \\t]?[A-Za-z]*)[ \\t:;#\\|]?[ \t]?\\+?(1)?[ \\t]?[ \\t\\(\\-]?(\\d{3})[ \\t\\)\\-]?[ \\t]?(\\d{3})[ \\t\\-]?(\\d{4})";
+        /* Match Results:
+         * 1: Number Description: Alphabetic characters prefixing phone number. 
+         *    Used to match phone number vs fax, etc.
+         * 2: Country calling code
+         * 3: Area code
+         * 4: First 3 digits of subscriber number
+         * 5: Last 4 digits of subscriber number
+         */
+        String regex = "([A-Za-z]*[ \\t]?[A-Za-z]*)[ \\t:;#\\|]?[ \t]?" 
+                     + "\\+?(1)?[ \\t]?[ \\t\\(\\-\\.]?(\\d{3})[ \\t\\)\\-\\.]?"
+                     + "[ \\t]?(\\d{3})[ \\t\\-\\.]?(\\d{4})";
         Pattern pattern = Pattern.compile(regex);
         
         // Create matcher object
@@ -49,25 +66,36 @@ class Contact implements ContactInfo {
         // Stores numbers that match pattern (phone, fax, etc.)
         ArrayList<String> numbers = new ArrayList<String>(1);
         while (matcher.find()) {
+            // Store number description
             numberDesc.add(matcher.group(1).toLowerCase());
+            // If country calling code included in phone number 
             if (matcher.group(2) != null) {
-                numbers.add(matcher.group(2) + matcher.group(3) + matcher.group(4) + matcher.group(5));
+                numbers.add(matcher.group(2) + matcher.group(3) 
+                            + matcher.group(4) + matcher.group(5));
             }
             else {
-                numbers.add(matcher.group(3) + matcher.group(4) + matcher.group(5));
+                numbers.add(matcher.group(3) + matcher.group(4) 
+                            + matcher.group(5));
             }
         }
 
+        // If only one number listed on card, assume as phone number
         if (numbers.size() == 1) {
             return numbers.get(0);
         }
         else if (numbers.size() > 1) {
+            /* If any number's corresponding number description contains "tel" 
+             * or "pho", assume it is the phone number
+             */
             for (int i = 0; i < numbers.size(); i ++) {
-                if (numberDesc.get(i).contains("tel") || numberDesc.get(i).contains("pho")) {
+                if (numberDesc.get(i).contains("tel") 
+                    || numberDesc.get(i).contains("pho")) {
                     return numbers.get(i);
                 }
             }
-            // In case of no number description matche, assume first number is correct number
+            /* In case of no number description contains "tel" or "pho", assume 
+             * first number is correct number
+             */
             return numbers.get(0);
         }
         else {
@@ -82,12 +110,15 @@ class Contact implements ContactInfo {
     public String parseEmail(String text) {
         //Can't parse comments i.e. "(.*)" with regex, need operational symmantics
         // Create regex to pattern match emails
-        String regex = "[A-Za-z\\d](\\.?[A-Za-z\\d\\-_])*@[A-Za-z\\d](\\.?[A-Za-z\\d\\-]+)*";
+        String regex = "[A-Za-z\\d](\\.?[A-Za-z\\d\\-_])*" 
+                     + "@[A-Za-z\\d](\\.?[A-Za-z\\d\\-]+)*";
         Pattern pattern = Pattern.compile(regex);
 
         // Create matcher object
         Matcher matcher = pattern.matcher(text);
 
+        // Return first match as email address
+        // Assuming no string will match the pattern not be an email address
         if (matcher.find()) {
             return matcher.group(0);
         }
@@ -105,22 +136,29 @@ interface BuisnessCardParser {
     ContactInfo getContactInfo(String document);
 }
 
+/*
+ * This class defines the BusinessCard object. The class stores only a single 
+ * getContactInfo method which takes a String and returns a Contact object.
+ */
 class BuisnessCard implements BuisnessCardParser {
     public ContactInfo getContactInfo(String document) {
-        ContactInfo contact = new Contact(document);
-        return contact;
+        return new Contact(document);
     }
 }
 
 class Driver {
     public static void main(String[] args) {
+        // Checks for an input String
         if (args.length > 0) {
+            // Define parser object
             BuisnessCard parser = new BuisnessCard();
+            // Parse business card text
             ContactInfo contact = parser.getContactInfo(args[0]);
-            //TODO: Return cat of strings printed in final
-            System.out.println("Name: " + contact.getName());
-            System.out.println("Phone: " + contact.getPhoneNumebr());
-            System.out.println("Email: " + contact.getEmailAddress());
+            String output = "Name: " + contact.getName() + "\nPhone: " 
+                            + contact.getPhoneNumebr() + "\nEmail: " 
+                            + contact.getEmailAddress();
+                            
+            System.out.println(output);
         }
         else {
             System.out.println("Error: No Input");
